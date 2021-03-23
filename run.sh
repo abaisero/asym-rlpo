240a582e6f044d1d8064e5319539afd7a00dff44
abaisero <andrea.baisero@gmail.com>
2021-03-22 21:09:01 -0400

    add preliminary run.sh script

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/zsh
+
+for i in $(seq 10); do
+  sem -j4 ./main.py fob-dqn
+  sem -j4 ./main.py foe-dqn
+  sem -j4 ./main.py poe-dqn
+  sem -j4 ./main.py poe-adqn
+done
+
+sem --wait
+
+exit 0
