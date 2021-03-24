d6b1dd7857f3cddb73acdfe7e6cd81a053314b2a
abaisero <andrea.baisero@gmail.com>
2021-03-23 21:31:52 -0400

    minor

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/zsh
 
 for i in $(seq 10); do
-  sem -j4 ./main.py fob-dqn
-  sem -j4 ./main.py foe-dqn
-  sem -j4 ./main.py poe-dqn
-  sem -j4 ./main.py poe-adqn
+  sem -j4 ./main.py fob-dqn "$@"
+  sem -j4 ./main.py foe-dqn "$@"
+  sem -j4 ./main.py poe-dqn "$@"
+  sem -j4 ./main.py poe-adqn "$@"
 done
 
 sem --wait
