a94a8efc85a43a102d2eb7c9d62a551d9725e2e0
abaisero <andrea.baisero@gmail.com>
2021-04-22 18:12:07 -0400

    add EpisodeBuffer tests

---
+++ tests/test_data.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from asym_rlpo.data import Episode, EpisodeBuffer
+
+
+def make_episode(num_timesteps: int):
+    starts = np.zeros(num_timesteps, dtype=bool)
+    starts[0] = True
+    dones = np.zeros(num_timesteps, dtype=bool)
+    dones[-1] = True
+
+    return Episode(
+        states=np.zeros(num_timesteps),
+        observations=np.zeros(num_timesteps),
+        actions=np.zeros(num_timesteps, dtype=int),
+        rewards=np.zeros(num_timesteps),
+        starts=starts,
+        dones=dones,
+    )
+
+
+def test_episode_buffer_max_timesteps():
+    episode_buffer = EpisodeBuffer(100)
+    assert episode_buffer.num_interactions() == 0
+    assert episode_buffer.num_episodes() == 0
+
+    episode_buffer.append_episode(make_episode(101))
+    assert episode_buffer.num_interactions() == 0
+    assert episode_buffer.num_episodes() == 0
+
+    episode_buffer.append_episode(make_episode(10))
+    assert episode_buffer.num_interactions() == 10
+    assert episode_buffer.num_episodes() == 1
+
+    episode_buffer.append_episode(make_episode(20))
+    assert episode_buffer.num_interactions() == 30
+    assert episode_buffer.num_episodes() == 2
+
+    episode_buffer.append_episode(make_episode(80))
+    assert episode_buffer.num_interactions() == 100
+    assert episode_buffer.num_episodes() == 2
+
+    episode_buffer.append_episode(make_episode(1))
+    assert episode_buffer.num_interactions() == 81
+    assert episode_buffer.num_episodes() == 2
