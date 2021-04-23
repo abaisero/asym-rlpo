1b13f38ee0679780352dbe43dc564a01f52d1f94
Brett Daley <brett-daley@users.noreply.github.com>
2021-04-23 00:32:49 -0400

    Finish GV models

--- asym_rlpo/models.py
+++ asym_rlpo/models.py
@@ -53,18 +53,6 @@ def make_models(
 
 
 def make_models_openai(env: gym.Env) -> nn.ModuleDict:
-    (input_dim,) = env.state_space.shape
-    q_model = nn.Sequential(
-        make_module('linear', 'leaky_relu', input_dim, 512),
-        nn.LeakyReLU(),
-        make_module('linear', 'leaky_relu', 512, 256),
-        nn.LeakyReLU(),
-        make_module('linear', 'linear', 256, env.action_space.n),
-    )
-
-    # action_model = EmbeddingRepresentation(env.action_space.n, 128)
-    # observation_model = MLPRepresentation(env.observation_space, 128)
-
     action_model = OneHotRepresentation(env.action_space)
     observation_model = IdentityRepresentation(env.observation_space)
     state_model = IdentityRepresentation(env.state_space)
@@ -74,6 +62,13 @@ def make_models_openai(env: gym.Env) -> nn.ModuleDict:
         observation_model,
         hidden_size=128,
         nonlinearity='tanh',
+    )
+    q_model = nn.Sequential(
+        make_module('linear', 'leaky_relu', state_model.dim, 512),
+        nn.LeakyReLU(),
+        make_module('linear', 'leaky_relu', 512, 256),
+        nn.LeakyReLU(),
+        make_module('linear', 'linear', 256, env.action_space.n),
     )
     qh_model = nn.Sequential(
         make_module('linear', 'leaky_relu', history_model.dim, 512),
@@ -118,11 +113,30 @@ def make_models_gv(env: gym.Env) -> nn.ModuleDict:
         hidden_size=128,
     )
     q_model = nn.Sequential(
-        nn.Linear(state_model.dim, 128),
+        make_module('linear', 'relu', state_model.dim, 128),
         nn.ReLU(),
-        nn.Linear(128, 128),
+        make_module('linear', 'relu', 128, 128),
         nn.ReLU(),
-        nn.Linear(128, env.action_space.n),
+        make_module('linear', 'linear', 128, env.action_space.n),
+    )
+    qh_model = nn.Sequential(
+        make_module('linear', 'relu', history_model.dim, 128),
+        nn.ReLU(),
+        make_module('linear', 'relu', 128, 128),
+        nn.ReLU(),
+        make_module('linear', 'linear', 128, env.action_space.n),
+    )
+    qhs_model = nn.Sequential(
+        make_module(
+            'linear',
+            'relu',
+            history_model.dim + state_model.dim,
+            128,
+        ),
+        nn.ReLU(),
+        make_module('linear', 'relu', 128, 128),
+        nn.ReLU(),
+        make_module('linear', 'linear', 128, env.action_space.n),
     )
     return nn.ModuleDict(
         {
@@ -131,31 +145,7 @@ def make_models_gv(env: gym.Env) -> nn.ModuleDict:
             'state_model': state_model,
             'history_model': history_model,
             'q_model': q_model,
+            'qh_model': qh_model,
+            'qhs_model': qhs_model,
         }
     )
-
-
-# poe-dqn
-# def make_models_gv(env: gym.Env) -> nn.ModuleDict:
-#     action_model = EmbeddingRepresentation(env.action_space.n, 64)
-#     observation_model = GV_ObservationRepresentation(env.observation_space)
-#     history_model = RNNHistoryRepresentation(
-#         action_model,
-#         observation_model,
-#         hidden_size=128,
-#     )
-#     q_model = nn.Sequential(
-#         nn.Linear(history_model.dim, 128),
-#         nn.ReLU(),
-#         nn.Linear(128, 128),
-#         nn.ReLU(),
-#         nn.Linear(128, env.action_space.n),
-#     )
-#     models = nn.ModuleDict(
-#         {
-#             'action_model': action_model,
-#             'observation_model': observation_model,
-#             'history_model': history_model,
-#             'q_model': q_model,
-#         }
-#     )
