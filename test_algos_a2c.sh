03e2dbb055c0294df76dccddb5b82d48526d1959
abaisero <andrea.baisero@gmail.com>
2021-04-24 21:19:40 -0400

    fix integration tests

--- test_algos_a2c.sh
+++ test_algos_a2c.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 export WANDB_MODE=dryrun
+export WANDB_CONSOLE=off
+export WANDB_SILENT=true
 
 envs=(PO-pos-CartPole-v1 ../gym-gridverse/yaml/gv_empty.4x4.yaml)
 algos=(sym-a2c)
 
 args=(
-  --max-simulation-timesteps 11_000
+  --max-simulation-timesteps 500
 )
 
 for env in ${envs[@]}; do
   for algo in ${algos[@]}; do
     echo ./main_a2c.py $env $algo ${args[@]}
-    ./main_a2c.py $env $algo ${args[@]} &> /dev/null
-    [[ $? ]] && echo "SUCCESS" || echo "FAIL"
+    python -W ignore ./main_a2c.py $env $algo ${args[@]} > /dev/null
+    [ $? -eq 0 ] && echo "SUCCESS" || echo "FAIL"
   done
 done
 
--- test_algos_dqn.sh
+++ test_algos_dqn.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 export WANDB_MODE=dryrun
+export WANDB_CONSOLE=off
+export WANDB_SILENT=true
 
 envs=(PO-pos-CartPole-v1 ../gym-gridverse/yaml/gv_empty.4x4.yaml)
 algos=(fob-dqn foe-dqn poe-dqn poe-adqn)
 
 args=(
-  --max-simulation-timesteps 11_000
+  --episode-buffer-prepopulate-timesteps 100 --max-simulation-timesteps 500
 )
 
 for env in ${envs[@]}; do
   for algo in ${algos[@]}; do
     echo ./main_dqn.py $env $algo ${args[@]}
-    ./main_dqn.py $env $algo ${args[@]} &> /dev/null
-    [[ $? ]] && echo "SUCCESS" || echo "FAIL"
+    python -W ignore ./main_dqn.py $env $algo ${args[@]} > /dev/null
+    [ $? -eq 0 ] && echo "SUCCESS" || echo "FAIL"
   done
 done
 
