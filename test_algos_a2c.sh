0f213a6b7a4cf27f55ccd91f2f51e9316e8aae0e
abaisero <andrea.baisero@gmail.com>
2021-09-23 13:33:40 -0400

    update integration tests

--- test_algos_a2c.sh
+++ test_algos_a2c.sh
@@ -13,22 +13,26 @@ args=(
   --simulation-num-episodes 2
   # --truncated-histories
   # --truncated-histories-n 10
+  --normalize-hs-features
+  --hs-features-dim 64
 )
 
+warnings="-W ignore"
+# warnings=""
+
+debug="-m ipdb"
+debug=""
+
+[[ "$#" -eq 0 ]] && silent=1 || silent=0
 [[ "$#" -eq 0 ]] && echo "running without standard output" || echo "running with standard output"
 echo
 
 for env in ${envs[@]}; do
   for algo in ${algos[@]}; do
-    echo ./main_a2c.py $env $algo ${args[@]}
-
-    if [[ "$#" -eq 0 ]]; then
-      python -W ignore ./main_a2c.py $env $algo ${args[@]} > /dev/null
-    else
-      python -W ignore ./main_a2c.py $env $algo ${args[@]}
-    fi
-
-    [[ $? -eq 0 ]] && echo "SUCCESS" || echo "FAIL"
+    cmd="python $warnings $debug ./main_a2c.py $env $algo ${args[@]}"
+    echo $cmd
+    [[ "$silent" -eq 1 ]] && $cmd > /dev/null || $cmd
+    [[ "$?" -eq 0 ]] && echo "SUCCESS" || echo "FAIL"
   done
 done
 
